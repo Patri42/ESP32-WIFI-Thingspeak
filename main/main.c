99720f39c152c02eb06314e7238fe81ff2a62fcf
Patrick Borzych <123195888+Patri42@users.noreply.github.com>
2023-10-06 13:02:50 +0200

    update

--- main/main.c
+++ main/main.c
@@ -19,46 +19,74 @@ char api_key[] = "IWCH9QP5IUIQOI0Z";
 
 char message[] = "Hello This is a test message";
 
+int pulseData[] = {70, 72, 68, 74, 76, 73, 71};
+int oximetryData[] = {97, 98, 96, 99, 95, 96, 98};
+int dataSize = sizeof(pulseData) / sizeof(pulseData[0]);
+int currentIndex = 0;
+
+unsigned int getPulseData(void)
+{
+	int data = pulseData[currentIndex];
+	currentIndex = (currentIndex + 1) % dataSize;
+    return data;
+}
+
+unsigned int getOximetryData(void)
+{
+	int data = oximetryData[currentIndex];
+	currentIndex = (currentIndex + 1) % dataSize;
+    return data;
+}
+
+
 void thingspeak_send_data(void *pvParameters)
 {
-
-    char thingspeak_url[200];
-    snprintf(thingspeak_url,
-             sizeof(thingspeak_url),
-             "%s%s%s",
-             "https://api.thingspeak.com/update?api_key=",
-             api_key,
-             "&field1=60&field2=99");
-
-    esp_http_client_config_t config = {
-        .url = thingspeak_url,
-        .method = HTTP_METHOD_GET   };
-
-    esp_http_client_handle_t client = esp_http_client_init(&config);
-    esp_http_client_set_header(client, "Content-Type", "application/x-www-form-urlencoded");
-
-
-
-    esp_err_t err = esp_http_client_perform(client);
-
-    if (err == ESP_OK)
+    while (1) // Ensuring repeated data sending
     {
-        int status_code = esp_http_client_get_status_code(client);
-        if (status_code == 201)
+        unsigned int pulse = getPulseData();
+        unsigned int oximetry = getOximetryData();
+        
+        char thingspeak_url[200];
+        snprintf(thingspeak_url,
+                 sizeof(thingspeak_url),
+                 "%s%s%s%d%s%d",
+                 "https://api.thingspeak.com/update?api_key=",
+                 api_key,
+                 "&field1=",
+                 pulse,
+                 "&field2=",
+                 oximetry);
+
+        esp_http_client_config_t config = {
+            .url = thingspeak_url,
+            .method = HTTP_METHOD_GET
+        };
+
+        esp_http_client_handle_t client = esp_http_client_init(&config);
+        esp_http_client_set_header(client, "Content-Type", "application/x-www-form-urlencoded");
+
+        esp_err_t err = esp_http_client_perform(client);
+
+        if (err == ESP_OK)
         {
-            ESP_LOGI(TAG, "Message sent Successfully");
+            int status_code = esp_http_client_get_status_code(client);
+            if (status_code == 200) // Note: ThingSpeak typically returns a 200 status on successful updates
+            {
+                ESP_LOGI(TAG, "Message sent Successfully");
+            }
+            else
+            {
+                ESP_LOGI(TAG, "Message sent Failed");
+            }
         }
         else
         {
-            ESP_LOGI(TAG, "Message sent Failed");
+            ESP_LOGE(TAG, "HTTP client error: %s", esp_err_to_name(err));
         }
+        esp_http_client_cleanup(client);
+
+        vTaskDelay(16000 / portTICK_PERIOD_MS); // Delay 16 seconds as per ThingSpeak rate limits
     }
-    else
-    {
-        ESP_LOGI(TAG, "Message sent Failed");
-    }
-    esp_http_client_cleanup(client);
-    vTaskDelete(NULL);
 }
 
 void app_main(void)
